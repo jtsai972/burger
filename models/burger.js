ca1b97e531e8458089f3c47aadce19ad0152e4a4
J Tsai <jtsai972@gmail.com>
2019-10-26 01:37:52 -0500

    fixing table names

--- models/burger.js
+++ models/burger.js
@@ -3,27 +3,27 @@ const orm = require("../config/orm");
 var burger = {
     //This is for printing all the burgers
     all: function(cb) {
-        orm.all("burger", function(res) {
+        orm.all("burgers", function(res) {
             //return the results to the function that calls this
             cb(res);
         });
     },
     // This is for creating a new burgers
     create: function(newBurger, cb){
-        orm.all("burger", newBurger, function(res) {
+        orm.create("burgers", newBurger, function(res) {
             cb(res);
         });
     },
     // This is for updating the burgers eaten status
     // Could change this to update burger name
     update: function(isEaten, id, cb){
-        orm.all("burger", isEaten, id, function(res) {
+        orm.update("burgers", isEaten, id, function(res) {
             cb(res);
         });
     },
     // This is for deleting a burger
     delete: function(id, cb){
-        orm.all("burger", id, function(res) {
+        orm.delete("burgers", id, function(res) {
             cb(res);
         });
     }
