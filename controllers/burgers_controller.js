7feb3d9ce0aa3eeb5046d62bf650efbecb7c8412
J Tsai <jtsai972@gmail.com>
2019-10-26 00:56:11 -0500

    finished setting up all the parts

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,16 @@ const burger = require("../models/burger");
 
 const router = express.Router();
 
-// Create routes
-router.get("/", function(req, res) {
+// // Create routes
+// router.get("/", function(req, res) {
     
-})
+// })
+
+// router.post();
+
+// router.put();
+
+// router.delete();
+
+
+module.exports = router;
