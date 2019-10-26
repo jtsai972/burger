ee2047529d32298897b5c21d5222172b9ae166bb
J Tsai <jtsai972@gmail.com>
2019-10-25 21:11:10 -0500

    Adding all my models stuff

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+// * REQUIREMENTS
+const mysql = require('mysql');
+const auth = require("./serverAuth")
+
+
+const connection = mysql.createConnection(auth);
+
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
