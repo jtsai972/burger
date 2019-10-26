5a380d047bd31e3769eeb79589fb946cda3d0e2b
J Tsai <jtsai972@gmail.com>
2019-10-25 21:11:56 -0500

    finishing most of the models

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const burger = require("../models/burger");
+
+const router = express.Router();
+
+// Create routes
+router.get("/", function(req, res) {
+    
+})
