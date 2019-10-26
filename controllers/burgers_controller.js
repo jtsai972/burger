35d78d9822a70b814e6100df8b6fd4c249fbfc80
J Tsai <jtsai972@gmail.com>
2019-10-26 01:37:40 -0500

    added the preliminary routes for the app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,16 +3,44 @@ const burger = require("../models/burger");
 
 const router = express.Router();
 
-// // Create routes
-// router.get("/", function(req, res) {
-    
-// })
+// * HANDLING GET REQUEST: return all burgers in the db
+router.get("/", function(req, res) {
+    burger.all(function(data){
+      var handlebarsObj = {
+        burgers: data
+      };
+      console.log(handlebarsObj);
+      res.render("index", handlebarsObj);
+    })
+})
 
-// router.post();
+// * HANDLING POST REQUEST: 
+router.post("/api/burgers", function(req, res) {
+  burger.create({burger_name: req.body.name}, function(result) {
+    res.json({ id: result.insertId })
+  })
+});
 
-// router.put();
+// * HANDLING PUT REQUEST:
+router.put("/api/burgers/:id", function(req, res) {
+  burger.update(
+    {devoured: 1}, 
+    { id: req.params.id}, 
+    function(result) {
+      return result.changedRows === 0 ? 
+        res.status(404).end() : 
+        res.status(200).end();
+    })
+});
 
-// router.delete();
+// * HANDLING DELETE REQUEST:
+router.delete("/api/burgers/:id", function(req, res) {
+  burger.delete({ id: req.params.id}, function(result) {
+    return result.changedRows === 0 ? 
+        res.status(404).end() : 
+        res.status(200).end();
+  })
+});
 
 
 module.exports = router;
