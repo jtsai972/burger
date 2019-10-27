db2a24fc22ea7dc21da0d82edc5d92a9d958a1fd
J Tsai <jtsai972@gmail.com>
2019-10-26 23:49:11 -0500

    commenting out a console.log

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,7 +9,7 @@ router.get("/", function(req, res) {
       var handlebarsObj = {
         burgers: data
       };
-      console.log(handlebarsObj);
+      //console.log(handlebarsObj);
       res.render("index", handlebarsObj);
     })
 })
