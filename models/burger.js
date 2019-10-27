cd090d19f81fed3e384e3c41be992457b4c8c326
J Tsai <jtsai972@gmail.com>
2019-10-26 23:50:16 -0500

    fixing the names of the calls (i.e. orm.all -> orm.selectAll)

--- models/burger.js
+++ models/burger.js
@@ -3,27 +3,27 @@ const orm = require("../config/orm");
 var burger = {
     //This is for printing all the burgers
     all: function(cb) {
-        orm.all("burgers", function(res) {
+        orm.selectAll("burgers", function(res) {
             //return the results to the function that calls this
             cb(res);
         });
     },
     // This is for creating a new burgers
     create: function(newBurger, cb){
-        orm.create("burgers", newBurger, function(res) {
+        orm.insertOne("burgers", newBurger, function(res) {
             cb(res);
         });
     },
     // This is for updating the burgers eaten status
     // Could change this to update burger name
     update: function(isEaten, id, cb){
-        orm.update("burgers", isEaten, id, function(res) {
+        orm.updateOne("burgers", isEaten, id, function(res) {
             cb(res);
         });
     },
     // This is for deleting a burger
     delete: function(id, cb){
-        orm.delete("burgers", id, function(res) {
+        orm.deleteOne("burgers", id, function(res) {
             cb(res);
         });
     }
