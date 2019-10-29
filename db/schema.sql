6256085417797300c2f99ed494e7e7b29d6b8751
J Tsai <jtsai972@gmail.com>
2019-10-29 17:44:21 -0500

    updating schemas

--- db/schema.sql
+++ db/schema.sql
@@ -8,10 +8,10 @@ CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
     devoured tinyint NOT NULL DEFAULT 0, 
-    isRated tinyint NOT NULL DEFAULT 0, 
-    rating tinyint NULL,
-    isReviewed tinyint NOT NULL DEFAULT 0,
-    review VARCHAR(255) NULL,
+    -- isRated tinyint NOT NULL DEFAULT 0, 
+    -- rating tinyint NULL,
+    -- isReviewed tinyint NOT NULL DEFAULT 0,
+    -- review VARCHAR(255) NULL,
     PRIMARY KEY (id)
 );
 
